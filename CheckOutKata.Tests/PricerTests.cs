20a65ff4bd086afec41c99cdb884a8b9ddc50088
Jason Holloway <miceeatnicerice@hotmail.com>
2017-03-29 13:54:01 +0100

    Pricer uses simple, single pricing strategy via passed context; throws InvalidOperationException when no strategy findable

--- CheckOutKata.Tests/PricerTests.cs
+++ CheckOutKata.Tests/PricerTests.cs
@@ -25,24 +25,73 @@ namespace CheckOutKata.Tests
     }
 
 
-    public class Pricer : IPricer
+
+    public delegate bool PricingStrategy(Context x);
+
+
+    public class Context
     {
-        public decimal GetPrice(IEnumerable<SKU> skus) {
-            return 0M;
-        }
+        public Stack<SKU> SKUs = new Stack<SKU>(); //simpler to use Stack for time being
+        public decimal TotalPrice = 0M;
     }
 
 
+    public class Pricer : IPricer
+    {
+        PricingStrategy[] _strategies;
+
+        public Pricer(params PricingStrategy[] strategies) {
+            _strategies = strategies;
+        }
+
+        public decimal GetPrice(IEnumerable<SKU> skus) {            
+            var x = new Context {
+                SKUs = new Stack<SKU>(skus)
+            };
+
+            while(x.SKUs.Any()) {
+                _strategies.First(fn => fn(x)); //enumerates through strategies, executing them till one returns true
+            }                                   //this will also throw if no strategy found
+
+            return x.TotalPrice;
+        }
+    }
+
+    
+    
 
 
     public class PricerTests
     {        
-        [Fact(DisplayName = "Pricer returns price")]
-        public void Pricer_ReturnsPrice() {
+        [Fact(DisplayName = "Pricer throws when no strategy")]
+        public void Pricer_Throws_WhenNoStrategy() {
             var pricer = new Pricer();
-            var price = pricer.GetPrice(new[] { new SKU('A'), new SKU('B'), new SKU('C') });            
+
+            Should.Throw<InvalidOperationException>(() => {
+                pricer.GetPrice(new[] { new SKU('A'), new SKU('B'), new SKU('C') });
+            });         
         }
-                
+
+        
+        [Fact(DisplayName = "Pricer uses single PricingStrategy")]
+        public void Pricer_UsesSinglePricingStrategy() {
+            var pricer = new Pricer(DummyStrategy);
+
+            var price = pricer.GetPrice(new[] { new SKU('A'), new SKU('B'), new SKU('C'), new SKU('D') });
+
+            price.ShouldBe(6M);
+        }
+
+
+        static bool DummyStrategy(Context x) {
+            if(x.SKUs.Pop().Char > 'B') x.TotalPrice += 2;
+            else x.TotalPrice += 1;
+            
+            return true;
+        }
+
+
+
 
     }
 }
