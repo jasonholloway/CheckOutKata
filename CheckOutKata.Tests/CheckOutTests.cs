e13f7e1fcf4612bdd0d7bc716a6bec4be7947e11
Jason Holloway <miceeatnicerice@hotmail.com>
2017-03-29 12:25:38 +0100

    Added test project with xUnit and Shouldly

---
+++ CheckOutKata.Tests/CheckOutTests.cs
@@ -0,0 +1,20 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CheckOutKata.Tests
+{
+
+    public class CheckoutTests
+    {        
+        [Fact(DisplayName = "Blah")]
+        public void Blah() {            
+            throw new NotImplementedException();
+        }
+
+    }
+}
