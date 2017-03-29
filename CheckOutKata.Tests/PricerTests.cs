275538cbf7334ac6bd204843312d2e0ef3caeed8
Jason Holloway <miceeatnicerice@hotmail.com>
2017-03-29 12:42:52 +0100

    Pricer service returns price

---
+++ CheckOutKata.Tests/PricerTests.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CheckOutKata.Tests
+{
+
+    public struct SKU
+    {
+        public readonly char Char;
+
+        public SKU(char @char) {
+            Char = @char;
+        }
+    }
+
+
+    public interface IPricer
+    {
+        decimal GetPrice(IEnumerable<SKU> skus);
+    }
+
+
+    public class Pricer : IPricer
+    {
+        public decimal GetPrice(IEnumerable<SKU> skus) {
+            return 0M;
+        }
+    }
+
+
+
+
+    public class PricerTests
+    {        
+        [Fact(DisplayName = "Pricer returns price")]
+        public void Pricer_ReturnsPrice() {
+            var pricer = new Pricer();
+            var price = pricer.GetPrice(new[] { new SKU('A'), new SKU('B'), new SKU('C') });            
+        }
+                
+
+    }
+}
--- CheckOutKata/Pricer.cs
+++ CheckOutKata/Pricer.cs
@@ -6,7 +6,5 @@ using System.Threading.Tasks;
 
 namespace CheckOutKata
 {
-    public class CheckOut
-    {
-    }
+    //...
 }
